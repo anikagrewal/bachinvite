0cfda4afee94c105154540fe563f9e0f9a6b4906
anikagrewal <a.grewal.work@gmail.com>
2026-05-13 21:28:11 -0700

    vercel fix

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,11 +1,8 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-export const createClient = (cookieStore: ReadonlyRequestCookies) =>
-  createBrowserClient(
-    supabaseUrl!,
-    supabaseKey!,
-  );
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! // Make sure this matches your .env
+  )
+}
