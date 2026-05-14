81600518b5b79ccd077e89afd74edce0af638f37
anikagrewal <a.grewal.work@gmail.com>
2026-05-13 22:11:12 -0700

    connection

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,7 +3,7 @@ import { createBrowserClient } from '@supabase/ssr'
 export function createClient() {
   // Use empty strings as fallbacks so the build doesn't crash
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || '';
 
   return createBrowserClient(
     supabaseUrl,
